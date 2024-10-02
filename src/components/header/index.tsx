afea2ebd7e1bbc590c9e89c69867b49f89145bdc
gustavodscruz <gustavodiasdsc@gmail.com>
2024-10-02 00:52:09 -0300

    erro na chamada do image do astro

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,14 @@
-import Image from 'astro/components/Image.astro'
+import Image from 'astro:assets'
 import React from 'react'
+import MyImage from '../MyImage'
 
 const Header = () => {
   return (
     <div className='bg-primary w-full py-14 border-b-black border-b-8'>
         <div className="w-5/12 bg-white border-black border-2 m-auto h-24 flex">
             <div className="w-6/12 h-full flex gap-5 border-black border-2 items-center justify-start pl-3 hover:bg-gdsc-green transition-all duration-300 cursor-pointer">
-                <img className='h-7' src="/icons/vector-1.svg" alt='Ícone' />
+                <Image src="/icons/vector-1.svg" alt="Ícone" width={ 500} height={300} />
+                {/* <MyImage src="/icons/vector-1.svg" alt="Ícone" /> */}
                 <h1 className='font-medium text-3xl'>gustavodscruz</h1>
             </div>
             <div className="w-3/12 flex border-black border-2 items-center justify-center hover:bg-gdsc-green transition-all duration-300 cursor-pointer">
